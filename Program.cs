c4235fb265d16e838c7a91ddad9372aba5b78322
TOVTC <veronica.tc.to@outlook.com>
2023-03-07 23:42:31 -0500

    added loops example

--- Program.cs
+++ Program.cs
@@ -135,6 +135,13 @@ namespace CatWorx.BadgeMaker
             employees.Add("barbara");
             employees.Add("billy");
             Console.WriteLine("My employees include {0}, {1}, {2}, and {3}", employees[0], employees[1], employees[2], employees[3]);
+        
+            // -----LOOPS--------------------------------------------------------------
+            Console.WriteLine("\nLOOPS");
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i]);
+            }
         }
     }
 }
