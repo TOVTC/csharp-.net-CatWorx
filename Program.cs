1581230d79d228afb356657516c0ad1550d45bac
TOVTC <veronica.tc.to@outlook.com>
2023-03-08 02:38:37 -0500

    added functionality for app to accept multiple inputs from command line

--- Program.cs
+++ Program.cs
@@ -13,16 +13,22 @@ namespace CatWorx.BadgeMaker
         static void Main(string[] args)
         {
             // intialize a list of string values called employees that already contains two strings
-            List<string> employees = new List<string>() {"adam", "amy"};
-            // add two new names to the employees list
-            employees.Add("barbara");
-            employees.Add("billy");
-            Console.WriteLine("Please enter a name :");
-            // get a name from the console and assign it to a variable
-            string input = Console.ReadLine() ?? ""; // .NET runtime halts here and waits for input before it continues executing on the next line
-            // the null coalescing operator ?? is similar to ternary
-            // ?? checks whether the value from the command line is null (because input can only be of type string) and replaces it with the value after the operator (here it's an empty string)
-            employees.Add(input);
+            List<string> employees = new List<string>();
+            // collect user values until the value is an empty string
+            while (true)
+            {
+                Console.WriteLine("Please enter a name :");
+                // get a name from the console and assign it to a variable
+                string input = Console.ReadLine() ?? ""; // .NET runtime halts here and waits for input before it continues executing on the next line
+                // the null coalescing operator ?? is similar to ternary
+                // ?? checks whether the value from the command line is null (because input can only be of type string) and replaces it with the value after the operator (here it's an empty string)
+                if (input == "")
+                {
+                    // break the while loop if the user hits enter without typing a name
+                    break;
+                }
+                employees.Add(input);
+            }
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
